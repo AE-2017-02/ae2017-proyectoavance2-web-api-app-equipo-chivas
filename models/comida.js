f5a059c0e76c28159a46c65dd59fc576fe603a69
garlop <lagl95@hotmail.com>
2017-11-22 00:50:31 -0700

    feat: modifying food model

--- models/comida.js
+++ models/comida.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 var comidaSchema = {
         nombre: { type: String, required:true },
-        ingred: [{ idIngred: { type: mongoose.Schema.Types.ObjectId, required:true , ref: "Ingrediente" },
-                   cant: { type: Number, required:true }
+        ingred: [{ _id: { type: mongoose.Schema.Types.ObjectId, required:true , ref: "Ingrediente" },
+                   cant: { type: Number, required:true, default: 0 }
         }]           
 };
 
