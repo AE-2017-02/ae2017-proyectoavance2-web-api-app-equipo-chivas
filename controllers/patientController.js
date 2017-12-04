24fc6bf5815cb8fb5c581e592adc9a5d1004abb5
garlop <lagl95@hotmail.com>
2017-12-03 23:46:09 -0700

    a

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -230,13 +230,19 @@ module.exports.setPatientPantry = function(req, res, Paciente, Comida){
 			var j = 0;
 			
 			var funcion1 = function(element){
-				Paciente.update({ $and : [{"_id": _id}, {"despensa": { $elemMatch: {"fecha":fecha}}} , {"despensa": { $elemMatch: {"comidaTiempo": result.tipo}}}, {"despensa" : {$elemMatch:{"menuId" : idComida}}}]}, {$addToSet: {"despensa.$.ingredientes" : element._id.nombre } }, {upsert: true}).exec(function(err, result){
+				Paciente.update({"_id": _id}, {$addToSet: {"despensa" : {"fecha": fecha, "comidaTiempo": result.tipo, "menuId": idComida, ingredientes: []}}}).exec(function(err, result){
 					if(err){
 						return res.status(status.INTERNAL_SERVER_ERROR).json({error: err.toString()});
 					}
-					console.log(result);
-					i++;
-					funcion2();	
+					
+					Paciente.update({ $and : [{"_id": _id}, {"despensa": { $elemMatch: {"fecha":fecha}}} , {"despensa": { $elemMatch: {"comidaTiempo": result.tipo}}}, {"despensa" : {$elemMatch:{"menuId" : idComida}}}]}, {$addToSet: {"despensa.$.ingredientes" : element._id.nombre } }).exec(function(error, resulta){
+						if(error){
+							return res.status(status.INTERNAL_SERVER_ERROR).json({error: error.toString()});
+							console.log(result);
+							i++;
+							funcion2();	
+						}
+					});
 				});	
 			}
 			
